13504660ad06b40c02af3b7d11815d901a2756cc
TheOratorEse <oratoreseiwi@gmail.com>
2023-09-18 23:30:16 +0100

    added container items

---
+++ android/app/src/main/kotlin/com/example/controlswitch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.controlswitch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sections.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color blueColor = const Color.fromARGB(255, 54, 30, 156);
+  static Color black = Colors.black;
+  static Color white = Colors.white;
+  static Color grey = Colors.grey;
+  static Color yellow = const Color.fromARGB(255, 255, 210, 12);
+  static Color green = const Color.fromARGB(255, 122, 245, 128);
+  static Color purple = Colors.deepPurple;
+}
